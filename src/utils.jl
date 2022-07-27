88a18b670ae9f2514f2aab7cd03ab972fc211be7
Aaron Trowbridge <aaron.j.trowbridge@gmail.com>
2022-07-27 16:38:23 -0400

    more slice/index documenation edits

--- src/utils.jl
+++ src/utils.jl
@@ -4,29 +4,40 @@ export index
 export slice
 
 """
-    this module contains helper functions for indexing and taking slices of the full problem variable vector
+this module contains helper functions for indexing and taking slices of the full problem variable vector
 
-    for the problem vector Z = [z₁, z₂, ..., zT]
-        where zₜ = [xₜ, uₜ]
-            where xₜ is the augmented state vector:
-                [ψ̃¹ₜ, ψ̃²ₜ, ..., ψ̃ⁿₜ, ∫aₜ, aₜ, daₜ, ..., dᶜaₜ]
-            with c = control_order
-        with dim(zₜ) = dim
+definitions:
 
-    Z[index(t, pos, dim)] = zₜ[pos]
-    Z[index(t, dim)]      = zₜ[dim]
+the problem vector: Z = [z₁, z₂, ..., zT]
 
-    Z[slice(t, pos1, pos2, dim)]      = zₜ[pos1:pos2]
-    Z[slice(t, pos, dim)]             = zₜ[1:pos]
-    Z[slice(t, dim)]                  = zₜ[1:dim]
-    Z[slice(t, dim; stretch=stretch)] = zₜ[1:(dim + stretch)]
+    knot point:
+        zₜ = [xₜ, uₜ]
 
-    the functions are also used to access the zₜ vectors, e.g.
+    augmented state vector:
+        xₜ = [ψ̃ₜ, ψ̃²ₜ, ..., ψ̃ⁿₜ, ∫aₜ, aₜ, daₜ, ..., dᶜ⁻¹aₜ]
 
-    zₜ[slice(i, isodim)]                             = ψ̃ⁱₜ
-    zₜ[n_wfn_states .+ slice(1, ncontrols)]          = ∫aₜ
-    zₜ[n_wfn_states .+ slice(2, ncontrols)]          = aₜ
-    zₜ[n_wfn_states .+ slice(augdim + 1, ncontrols)] = uₜ = ddaₜ
+    where:
+        c = control_order
+
+
+also, below, we use dim(zₜ) = dim
+
+examples:
+
+Z[index(t, pos, dim)] = zₜ[pos]
+Z[index(t, dim)]      = zₜ[dim]
+
+Z[slice(t, pos1, pos2, dim)]      = zₜ[pos1:pos2]
+Z[slice(t, pos, dim)]             = zₜ[1:pos]
+Z[slice(t, dim)]                  = zₜ[1:dim]
+Z[slice(t, dim; stretch=stretch)] = zₜ[1:(dim + stretch)]
+
+the functions are also used to access the zₜ vectors, e.g.
+
+zₜ[slice(i, isodim)]                             = ψ̃ⁱₜ
+zₜ[n_wfn_states .+ slice(1, ncontrols)]          = ∫aₜ
+zₜ[n_wfn_states .+ slice(2, ncontrols)]          = aₜ
+zₜ[n_wfn_states .+ slice(augdim + 1, ncontrols)] = uₜ = ddaₜ
 """
 
 index(t::Int, pos::Int, dim::Int) = (t - 1) * dim + pos
