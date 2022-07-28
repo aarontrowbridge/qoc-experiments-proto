ca544c9e25a8ddbba0499b29906a3904df9a6c4a
Aaron Trowbridge <aaron.j.trowbridge@gmail.com>
2022-07-28 17:16:39 -0400

    started running multimode solves

--- experiments/multimode/g0_to_g1.jl
+++ experiments/multimode/g0_to_g1.jl
@@ -8,6 +8,12 @@ hf = h5open(hf_path, "r")
 H_drift = copy(transpose(hf["H_drift"][:, :]))
 H_drives = [copy(transpose(hf["H_drives"][:, :, i])) for i = 1:size(hf["H_drives"], 3)]
 
+qubit_a_bounds = [0.018 * 2π, 0.018 * 2π]
+
+cavity_a_bounds = fill(0.03, length(H_drives) - 2)
+
+a_bounds = [qubit_a_bounds; cavity_a_bounds]
+
 ψ1 = vcat(transpose(hf["psi1"][:, :])...)
 ψf = vcat(transpose(hf["psif"][:, :])...)
 
@@ -20,19 +26,16 @@ system = MultiModeQubitSystem(
     ψf
 )
 
-Δt_init_traj = ts[2] - ts[1]
 
-controls = copy(transpose(hf["controls"][:,:]))
-
-init_traj = Trajectory(system, controls, Δt_init_traj)
 
 iter = parse(Int, ARGS[4])
 
+tol = 1e-8
+
 options = Options(
     max_iter = iter,
-    tol = 1.0e-8,
+    tol = tol,
     max_cpu_time = 100000.0,
-    linear_solver = "mumps"
 )
 
 T = parse(Int, ARGS[1])
@@ -42,6 +45,9 @@ R = parse(Float64, ARGS[3])
 traj_guess = parse(Bool, ARGS[5])
 
 if traj_guess
+    Δt_init_traj = ts[2] - ts[1]
+    controls = copy(transpose(hf["controls"][:,:]))
+    init_traj = Trajectory(system, controls, Δt_init_traj)
     Δt = Δt_init_traj
     T = length(ts)
 end
@@ -63,24 +69,25 @@ else
         T;
         Δt=Δt,
         R=R,
+        a_bound=a_bounds,
         pin_first_qstate=pin_first_qstate,
         options=options
     )
 end
 
-plot_path = "plots/multimode/g0_to_g1_T_$(T)_dt_$(Δt)_R_$(R)_iter_$(iter)" *
+plot_path = "plots/multimode/g0_to_g1_T_$(T)_dt_$(Δt)_R_$(R)_iter_$(iter)_tol_$(tol)" *
     (traj_guess ? "_init_traj" : "") * (pin_first_qstate ? "_pinned" : "") * ".png"
 
 plot_multimode_qubit(system, prob.trajectory, plot_path)
 
 solve!(prob)
 
-pico_controls = controls_matrix(prob.trajectory, prob.system)
-tlist = hcat(prob.trajectory.times...)
+# pico_controls = controls_matrix(prob.trajectory, prob.system)
+# tlist = hcat(prob.trajectory.times...)
 
-h5open("notebooks/controls_data.h5", "w") do hdf
-    hdf["controls"] = pico_controls
-    hdf["tlist"] = tlist
-end
+# h5open("notebooks/controls_data.h5", "w") do hdf
+#     hdf["controls"] = pico_controls
+#     hdf["tlist"] = tlist
+# end
 
 plot_multimode_qubit(system, prob.trajectory, plot_path)
