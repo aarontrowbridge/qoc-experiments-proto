956c6ac6262c4d5fc528e0648ed985355544bd11
Aaron Trowbridge <aaron.j.trowbridge@gmail.com>
2022-09-15 19:07:48 -0400

    docs structure

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,23 @@ makedocs(
     format=Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
     ),
+    pages=[
+        "Introduction" => "intro.md",
+        "Getting Started" => "getting_started.md",
+        "Manual" => [
+            "Systems" => "systems.md",
+            "Objectives" => "objectives.md",
+            "Costs" => "costs.md",
+            "Constraints" => "constraints.md",
+            "Integrators" => "integrators.md",
+            "Problems" => "problems.md",
+            "Iterative Learning Control" => "ilc.md",
+            "Trajectories" => "trajectories.md",
+            "Plotting" => "plotting.md",
+        ],
+        "Examples" => "examples.md",
+        "API" => "api.md",
+    ],
 )
 
 deploydocs(
